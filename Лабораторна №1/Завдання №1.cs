ce69dab97cc311bed0781bffac6ce79d7e87e096
Sofusia <92271369+Sofusia@users.noreply.github.com>
2021-10-20 20:09:54 +0300

    Лабораторна готова

---
+++ Лабораторна №1/Завдання №1.cs
@@ -0,0 +1,29 @@
+﻿using System;
+using static System.Console;
+using static System.Math;
+class Program
+{
+    static void Main()
+    {
+        double x, y, z, a, b;
+        Random rnd = new Random();
+        Write(" x = "); x = rnd.Next(-100, 101); WriteLine(x.ToString());
+        Write(" y = "); y = rnd.Next(-100, 101); WriteLine(y.ToString());
+        Write(" z = "); z = rnd.Next(-100, 101); WriteLine(z.ToString());
+
+        if (Sqrt(Pow(x, 2) + x * Pow(y, 2) + Pow(x, 2) * z) >= 0)
+        {
+            a = 1 + Sin(x) / (Sqrt(Pow(x, 2) + x * Pow(y, 2) + Pow(x, 2) * z));
+
+            if (y + z / a != 0 && a != 0)
+            {
+                b = Log(Pow(a, 3) + Pow(x, 2)) / (x + a / (y + z / a));
+                WriteLine(" a = " + a.ToString());
+                WriteLine(" b = " + b.ToString());
+            }
+        }
+        else
+            WriteLine(" Error...");
+
+    }
+}
---
+++ Лабораторна №1/Завдання №2.cs
@@ -0,0 +1,29 @@
+﻿using System;
+using static System.Console;
+class Program
+{
+    static void Main(string[] args)
+    
+    {
+        Random rnd = new Random();
+        long n = rnd.Next(2, 1000000001); WriteLine(" n = " + n);
+        double a = 4, p = 2, k = 2;
+        for (double Merc = 2; Merc < n; p++)
+        {
+            if (p == 2)
+            {
+                Merc = a - 1;
+                WriteLine(" Число Мерсена = " + Merc);
+            }
+            else if (k <= p && p % k != 0)
+            {
+                Merc = a - 1;
+                WriteLine(" Число Мерсена = " + Merc);
+            }
+            else
+                WriteLine(" Число p не просте: p =  " + p);
+            a *= 2;
+            
+        }
+    }
+}
